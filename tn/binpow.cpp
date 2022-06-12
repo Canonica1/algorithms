6070b6df26080f714e7a0c83121d676ea0ec75f8
Canonica1 <88035227+Canonica1@users.noreply.github.com>
2022-06-12 14:17:48 +0300

    Create binpow.cpp

---
+++ tn/binpow.cpp
@@ -0,0 +1,21 @@
+ll binpow(ll a, ll n) {
+	ll res = 1;
+	while (n != 0) {
+		if (n & 1)
+			res = res * a % MOD;
+		a = a * a % MOD;
+		n >>= 1;
+	}
+	return res;
+}
+
+int binpow(int a, int n) {
+    int res = 1;
+    while (n != 0) {
+        if (n & 1)
+            res = res * a;
+        a = a * a;
+        n >>= 1;
+    }
+    return res;
+}
