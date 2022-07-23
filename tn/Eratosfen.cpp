507d4ab7a638623706c96ca7a8ef601faada8af9
Canonica1 <88035227+Canonica1@users.noreply.github.com>
2022-07-23 12:54:25 +0300

    Update Eratosfen.cpp

--- tn/Eratosfen.cpp
+++ tn/Eratosfen.cpp
@@ -1,11 +1,23 @@
-    vector<bool> a(n + 1, true);
-    for (ll i = 2; i<= n; i++)
-    {
-        if (a[i])
-        {
-            for (ll j = i * i; j <= n; j += i)
-            {
-                a[j] = false;
-            }
-        }
-    }
+vector<int> smallest_factor;
+vector<bool> prime;
+vector<int> primes;
+
+void sieve(int maximum) {
+	maximum = max(maximum, 1ll);
+	smallest_factor.assign(maximum + 1, 0);
+	prime.assign(maximum + 1, true);
+	prime[0] = prime[1] = false;
+	primes = {};
+
+	for (int p = 2; p <= maximum; p++)
+		if (prime[p]) {
+			smallest_factor[p] = p;
+			primes.push_back(p);
+
+			for (int64_t i = int64_t(p) * p; i <= maximum; i += p)
+				if (prime[i]) {
+					prime[i] = false;
+					smallest_factor[i] = p;
+				}
+		}
+}
