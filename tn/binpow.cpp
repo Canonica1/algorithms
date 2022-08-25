4a5923e56b9e5e62687ab43a0dc805bab53ae465
Canonica1 <88035227+Canonica1@users.noreply.github.com>
2022-08-25 21:43:15 +0300

    Update binpow.cpp

--- tn/binpow.cpp
+++ tn/binpow.cpp
@@ -1,12 +1,12 @@
-ll binpow(ll a, ll n) {
-	ll res = 1;
-	while (n != 0) {
-		if (n & 1)
-			res = res * a % MOD;
-		a = a * a % MOD;
-		n >>= 1;
-	}
-	return res;
+int binpow(int a, int n) {
+    int res = 1;
+    while (n != 0) {
+        if (n & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+    return res;
 }
 
 int binpow(int a, int n) {
