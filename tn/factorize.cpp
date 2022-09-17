09014b876d12d3ddb3e03ba1754e85b998682088
Canonica1 <88035227+Canonica1@users.noreply.github.com>
2022-09-17 10:50:18 +0300

    Update factorize.cpp

--- tn/factorize.cpp
+++ tn/factorize.cpp
@@ -1,13 +1,29 @@
-vector<int> factorize(int x) {
-	vector<int> factors;
-	for (int i = 2; i <= sqrt(x); i++) {
-		while (x % i == 0) {
-			factors.push_back(i);
-			x /= i;
-		}
-	} 
-	if (x != 1) {
-		factors.push_back(x);
-	}
-	return factors;
+vector<bool> isprime(N, 1);
+vector<int> primes;
+void sieve() {
+    for (int i = 2; i <= N; ++i) {
+        if (isprime[i]) {
+            primes.push_back(i);
+        }
+        for (auto p : primes) {
+            if (i * p > N) {
+                break;
+            }
+            isprime[i * p] = 0;
+        }
+    }
+}
+int factorize(int x) {
+    int factors = 0;
+    for (auto i : primes) {
+        if (i * i> x) break;
+        while (x % i == 0) {
+            factors++;
+            x /= i;
+        }
+    }
+    if (x != 1) {
+        factors++;
+    }
+    return factors;
 }
