59afbe83a6213e8fa24e5626fb301678004278b0
Canonica1 <88035227+Canonica1@users.noreply.github.com>
2023-02-07 18:49:52 +0300

    Update Z.cpp

--- combinatorics/Z.cpp
+++ combinatorics/Z.cpp
@@ -1,22 +1,22 @@
 
 struct Z {
-	ve<int> f;
-	ve<int> r;
+	vector<int> f;
+	vector<int> r;
 	void initFacts() {
 		f.assign(N, 0);
 		r.assign(N, 0);
 		f[0] = 1;
 		for (int i = 1; i < N; i++)
-			f[i] = i * f[i - 1] % MOD;
+			f[i] = i * f[i - 1] % mod;
 
-		r[N - 1] = binpow(f[N - 1], MOD - 2); // это binpow mod-2;
+		r[N - 1] = binpow(f[N - 1], mod - 2); // это binpow mod-2;
 		for (int i = N - 1; i >= 1; i--)
-			r[i - 1] = r[i] * i % MOD;
+			r[i - 1] = r[i] * i % mod;
 	}
 	int choose(int n, int k) {
-		return f[n] * r[n - k] % MOD * r[k] % MOD;
+		return f[n] * r[n - k] % mod * r[k] % mod;
 	}
 	int perm(int n, int k) {
-		return f[n] * r[n-k] % MOD;
+		return f[n] * r[n-k] % mod;
 	}
 };
